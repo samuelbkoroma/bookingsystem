ddddde82af0b7c6c5996d860429e6c1b0e031ad8
Power <samuelbkoroma7@gmail.com>
2024-04-22 13:53:01 +0000

    latest

--- src/config/firestore.js
+++ src/config/firestore.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,4 +22,4 @@ export const app = initializeApp(firebaseConfig);
 
 //initialize cloud Firestore and get a reference to the services
 export const db = getFirestore(app);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
