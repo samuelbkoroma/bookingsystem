fc2f22af0443b8db4e6155e1f48d1e67e90c3ad5
Power <samuelbkoroma7@gmail.com>
2024-04-22 13:37:59 +0000

    latest

--- src/config/firestore.js
+++ src/config/firestore.js
@@ -8,13 +8,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: "AIzaSyCCjTuTn4PhUn4-eki1R4Amrqptt9mo_GE",
+  authDomain: "bookings-8eb99.firebaseapp.com",
+  projectId: "bookings-8eb99",
+  storageBucket: "bookings-8eb99.appspot.com",
+  messagingSenderId: "901405037529",
+  appId: "1:901405037529:web:9a0ac39aaa1a819dd9bfc8",
+  measurementId: "G-RVNQZ0ZH1T",
 };
 
 // Initialize Firebase
